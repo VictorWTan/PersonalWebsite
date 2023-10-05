b310cee78b037eb6ae3d1fcc9a01d7b12129a9da
victortan <vtan8090@gmail.com>
2023-10-05 16:28:39 -0400

    Added experience, projects, and education section

---
+++ website.js
@@ -0,0 +1,24 @@
+let body = document.body
+let html = document.documentElement
+
+const arrowDisappear = () => {
+    let button = document.getElementsByClassName('button-container')[0]
+    let scrollTop = html.scrollTop
+    button.style.opacity = 1 - (scrollTop / button.offsetHeight / 3)
+    // console.log(`${button.style.opacity}, ${button.offsetHeight}, ${html.scrollTop}`)
+}
+
+const experienceAppear = () => {
+    let experience = document.getElementsByClassName('experience')
+    let scrollTop = html.scrollTop
+    // experience.style.opacity = 0 + ( scrollTop / experience.offsetHeight)
+    // console.log(`${experience.style.opacity}, ${experience.offsetHeight}, ${html.scrollTop}`)
+}
+
+const fadeInAndOut = () => {
+    arrowDisappear()
+    experienceAppear()
+}
+
+
+window.addEventListener('scroll', fadeInAndOut)
